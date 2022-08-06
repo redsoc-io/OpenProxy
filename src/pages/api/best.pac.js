ac854ffb034416f468069c5030a285ee56922d2f
midhunvnadh <midhun.vnadh5732@gmail.com>
2022-08-06 20:06:09 +0530

    Add proxy

--- src/pages/api/best.pac.js
+++ src/pages/api/best.pac.js
@@ -10,13 +10,27 @@ function write_proxy_pac(url) {
 }
 
 export default async function handler(req, res) {
+    const { filter_country, filter_proto } = req.query;
     const request = await fetch(`https://api.oproxy.ml/servers`);
     var data = await request.json()
-    const best_server = data.sort((a, b) => b.speed_score - a.speed_score);
+    var filter = data
+    if (filter_country) {
+        filter = filter.filter(x => x.data.country.toLowerCase() === filter_country.toLowerCase())
+        console.log(filter)
+    }
+    if (filter_proto) {
+        filter = filter.filter(x => x.proto.toLowerCase() === filter_proto.toLowerCase())
+    }
+    const best_server = filter.sort((a, b) => b.speed_score - a.speed_score);
     const first_n = best_server.slice(0, 10);
     const pick_random = first_n[Math.floor(Math.random() * first_n.length)];
-    const pac = write_proxy_pac(pick_random.url);
-    res.setHeader('Content-Type', 'application/x-ns-proxy-autoconfig');
-    res.setHeader("Content-Disposition", "attachment; filename=proxy.pac");
-    res.status(200).send(pac);
+    if (pick_random) {
+        const pac = write_proxy_pac(pick_random.url);
+        res.setHeader('Content-Type', 'application/x-ns-proxy-autoconfig');
+        //res.setHeader("Content-Disposition", "attachment; filename=proxy.pac");
+        res.status(200).send(pac);
+    }
+    else {
+        res.status(404).send('No proxy found');
+    }
 }
