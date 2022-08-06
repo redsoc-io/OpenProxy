29acff150906b7b8683ff40026a381b5543d64f1
midhunvnadh <midhun.vnadh5732@gmail.com>
2022-08-06 19:28:59 +0530

    Pick at random

--- src/pages/api/best.pac.js
+++ src/pages/api/best.pac.js
@@ -12,8 +12,10 @@ function write_proxy_pac(url) {
 export default async function handler(req, res) {
     const request = await fetch(`https://api.oproxy.ml/servers`);
     var data = await request.json()
-    const best_server = data.sort((a, b) => b.speed_score - a.speed_score)[0];
-    const pac = write_proxy_pac(best_server.url);
+    const best_server = data.sort((a, b) => b.speed_score - a.speed_score);
+    const first_n = best_server.slice(0, 10);
+    const pick_random = first_n[Math.floor(Math.random() * first_n.length)];
+    const pac = write_proxy_pac(pick_random.url);
     res.setHeader('Content-Type', 'application/x-ns-proxy-autoconfig');
     res.setHeader("Content-Disposition", "attachment; filename=proxy.pac");
     res.status(200).send(pac);
