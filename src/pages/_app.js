e58fe50dcccff1ee67ea050f8b5ce8bcc57ed0a3
midhunvnadh <midhun.vnadh5732@gmail.com>
2022-08-10 20:49:39 +0530

    Add google analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,23 @@
 import "../assets/app.css"
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Script
+      src="https://www.googletagmanager.com/gtag/js?id=G-5WREWT1MPD"
+      strategy="afterInteractive"
+    />
+    <Script id="google-analytics" strategy="afterInteractive">
+      {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-5WREWT1MPD');
+        `}
+    </Script>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
