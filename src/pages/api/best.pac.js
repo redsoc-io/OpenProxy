70dc6f0ddad14f6f89c708544d80cd6edfa7f131
midhunvnadh <midhun.vnadh5732@gmail.com>
2022-08-11 02:11:59 +0530

    Update pac config

--- src/pages/api/best.pac.js
+++ src/pages/api/best.pac.js
@@ -1,3 +1,6 @@
+import fetch from "node-fetch"
+import HttpsProxyAgent from "https-proxy-agent";
+
 function write_proxy_pac(url) {
     var split = url.split('://'),
         proto = split[0].toUpperCase(),
@@ -7,6 +10,18 @@ function write_proxy_pac(url) {
         return '${proto} ${url}';
     }
   `;
+}
+
+async function test_proxy(url) {
+    try {
+        const proxyAgent = new HttpsProxyAgent(url);
+        const response = await fetch('https://raw.githubusercontent.com/jamesward/play-load-tests/master/public/1mb.txt', { agent: proxyAgent, signal: AbortSignal.timeout(3000) });
+        return true
+    }
+    catch (e) {
+        // console.log(e)
+        return false
+    }
 }
 
 export default async function handler(req, res) {
@@ -22,12 +37,28 @@ export default async function handler(req, res) {
         filter = filter.filter(x => x.proto.toLowerCase() === filter_proto.toLowerCase())
     }
     const best_server = filter.sort((a, b) => b.speed_score - a.speed_score);
-    const first_n = best_server.slice(0, 10);
-    const pick_random = first_n[Math.floor(Math.random() * first_n.length)];
-    if (pick_random) {
-        const pac = write_proxy_pac(pick_random.url);
-        res.setHeader('Content-Type', 'application/x-ns-proxy-autoconfig');
-        res.setHeader("Content-Disposition", "attachment; filename=proxy.pac");
+    const first_n = best_server.slice(0, 20)
+
+    for (let i = first_n.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [first_n[i], first_n[j]] = [first_n[j], first_n[i]];
+    }
+
+    var pick = null;
+
+    while (pick === null && first_n.length > 0) {
+        console.log(first_n.length)
+        const server = first_n.shift();
+        const test_proxy_ret = await test_proxy(server.url);
+        if (test_proxy_ret) {
+            pick = server;
+        }
+    }
+
+    if (pick) {
+        const pac = write_proxy_pac(pick.url);
+        //res.setHeader('Content-Type', 'application/x-ns-proxy-autoconfig');
+        //res.setHeader("Content-Disposition", "attachment; filename=proxy.pac");
         res.status(200).send(pac);
     }
     else {
