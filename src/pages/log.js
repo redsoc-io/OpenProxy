09a977aa175a97af31c38cfbd3ea34e60edb13ed
midhunvnadh <midhun.vnadh5732@gmail.com>
2022-08-12 15:26:33 +0530

    Design Changes

--- src/pages/log.js
+++ src/pages/log.js
@@ -14,7 +14,7 @@ export default function LogPage() {
                     src={"/api/last_log"}
                     className='lg:w-3/4 m-0 h-screen overflow-scroll w-full bg-white rounded-md text-sm border-2 shadow-md bg-black/70 text-white'
                     onLoad={(e) => {
-                        e.target.contentWindow.document.querySelector("pre").style.color = '#fff';
+                        e.target.contentWindow.document.querySelector("body").style.color = '#fff';
                     }}
                 ></iframe>
             </div>
