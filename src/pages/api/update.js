20c0a9c02c1fc3339b9f89af582a6ef0ca916648
midhunvnadh <midhun.vnadh5732@gmail.com>
2024-04-22 00:29:37 +0530

    Updater

--- src/pages/api/update.js
+++ src/pages/api/update.js
@@ -1,6 +1,6 @@
 import db from "../../lib/mongo";
 import downloadFileWithProxy from "../../lib/proxyDownload";
-const n = 100;
+const n = 500;
 
 export default async function handler(req, res) {
   const mg = await db();
