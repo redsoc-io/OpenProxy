742f97cc65817320a622ab87e6fc30ceea85322a
midhunvnadh <midhun.vnadh5732@gmail.com>
2024-04-21 20:21:13 +0530

    max duration

--- src/pages/api/cron.js
+++ src/pages/api/cron.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-function resend() {
+function resend(base_url) {
   setTimeout(() => {
     try {
       axios.get(`${base_url}/api/cron`);
     } catch (e) {
       console.log(e);
-      resend();
+      resend(base_url);
     }
   }, 8000);
 }
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
     const base_url = `${protocol}://${host_base}`;
 
     try {
-      resend();
+      resend(base_url);
       await axios.get(`${base_url}/api/update`);
     } catch (e) {
       console.log(e);
