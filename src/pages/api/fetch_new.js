e92d46586efb8f9e26fab475b584844cb7d1d8f9
midhunvnadh <midhun.vnadh5732@gmail.com>
2024-04-22 17:13:34 +0530

    Trying to fix the server finder

--- src/pages/api/fetch_new.js
+++ src/pages/api/fetch_new.js
@@ -12,14 +12,21 @@ function sha2hash(text) {
   return crypto.createHash("sha256").update(text).digest("hex");
 }
 
-async function get_all_docs() {
-  return (await db()).find({}, { projection: { _id: 1 } }).toArray();
+async function get_existing_servers() {
+  const sv = await db();
+  const servers = await sv.find({}, { projection: { _id: 1 } }).toArray();
+  const docs = servers.map(({ _id }) => {
+    return _id;
+  });
+  const set = new Set(docs);
+  return set;
 }
 
-export default async function handler(req, res) {
+async function get_new_servers() {
   var dat = sources.map(async (source) => {
     const request = await fetch(source.url);
     var data = await request.text();
+
     data = data.split("\n");
     data = data.map((line) => {
       if (source.type === "http") return `http://${line}`;
@@ -32,14 +39,15 @@ export default async function handler(req, res) {
   });
 
   dat = await Promise.all(dat);
+
   dat = dat.flat();
   dat = dat.filter((line) => isValidUrl(line));
   var len = dat.length;
-  dat = Array.from(new Set(dat));
+  dat = new Set(dat);
+  dat = Array.from(dat);
   len = len - dat.length;
-  dat = dat.sort();
 
-  const insert = dat.map((server) => {
+  var insert = dat.map(async (server) => {
     const hash = sha2hash(server);
     return {
       _id: hash,
@@ -51,22 +59,25 @@ export default async function handler(req, res) {
     };
   });
 
-  const docs = (await get_all_docs()).map(({ _id }) => {
-    return _id;
+  insert = await Promise.all(insert);
+
+  return insert;
+}
+
+export default async function handler(req, res) {
+  const prm = [get_existing_servers, get_new_servers].map(async (fn) => {
+    return await fn();
   });
 
-  const filtered = insert.filter(({ _id }) => {
-    return !docs.includes(_id);
-  });
+  const [docs, data] = await Promise.all(prm);
+
+  var filtered = data.filter(({ _id }) => !docs.has(_id));
 
   if (filtered.length > 0) {
     (await db()).insertMany(filtered);
   }
 
   res.status(200).json({
-    found: dat.length,
     added: filtered.length,
-    total_servers: docs.length + filtered.length,
-    removed_duplicates: len,
   });
 }
