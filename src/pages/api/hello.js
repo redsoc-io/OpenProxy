7bd5a96952879e89cb7868f12082b73bf1b276e3
midhunvnadh <midhun.vnadh5732@gmail.com>
2024-04-23 10:56:48 +0530

    show headers

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -4,5 +4,5 @@ export default async function handler(req, res) {
   const country = req.headers["x-vercel-ip-country"];
 
   console.log(`Hello from ${country || "??"} by ${from}`);
-  res.status(200).json({ from: from || "???", country: country || "??" });
+  res.status(200).json({ from: from || "???", country: country || "??", headers:req.headers });
 }
