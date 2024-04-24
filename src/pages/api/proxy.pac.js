e0f49199a2183e8e80230dcc9b47259e064c0b14
midhunvnadh <midhun.vnadh5732@gmail.com>
2024-04-24 11:34:38 +0530

    Fix proxy pac

--- src/pages/api/proxy.pac.js
+++ src/pages/api/proxy.pac.js
@@ -1,67 +1,57 @@
-import fetch from "node-fetch"
-import HttpsProxyAgent from "https-proxy-agent";
+import db from "../../lib/mongo";
 
-function write_proxy_pac(url) {
-    var split = url.split('://'),
-        proto = split[0].toUpperCase(),
-        url = split[1]
-    return `
+function write_proxy_pac(urls) {
+  urls = urls.map((url) => {
+    url = url
+      .replace("http://", "")
+      .replace("https://", "")
+      .replace("socks5://", "")
+      .replace("socks4://", "");
+    return `PROXY ${url}`;
+  });
+  return `
     function FindProxyForURL(url, host) {
-        return '${proto} ${url}';
+        return '${urls.join(";")}';
     }
   `;
 }
 
-async function test_proxy(url) {
-    try {
-        const proxyAgent = new HttpsProxyAgent(url);
-        const response = await fetch('https://raw.githubusercontent.com/jamesward/play-load-tests/master/public/1mb.txt', { agent: proxyAgent, signal: AbortSignal.timeout(3000) });
-        return true
-    }
-    catch (e) {
-        // console.log(e)
-        return false
-    }
-}
+var cached = {
+  urls: [],
+  last_updated: 0,
+};
 
 export default async function handler(req, res) {
-    const { filter_country, filter_proto } = req.query;
-    const request = await fetch(`https://openproxyproject.web.app/servers.json`);
-    var data = await request.json()
-    var filter = data
-    if (filter_country) {
-        filter = filter.filter(x => x.data.country.toLowerCase() === filter_country.toLowerCase())
-        console.log(filter)
-    }
-    if (filter_proto) {
-        filter = filter.filter(x => x.proto.toLowerCase() === filter_proto.toLowerCase())
-    }
-    const best_server = filter.sort((a, b) => b.speed_score - a.speed_score);
-    const first_n = best_server.slice(0, 20)
+  res.setHeader("Content-Type", "application/x-ns-proxy-autoconfig");
+  res.setHeader("Content-Disposition", "attachment; filename=proxy.pac");
 
-    for (let i = first_n.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [first_n[i], first_n[j]] = [first_n[j], first_n[i]];
-    }
+  if (cached.url && Date.now() - cached.last_updated < 1000 * 60 * 2) {
+    console.log("Serving cached proxy.pac");
+    res.status(200).send(write_proxy_pac(cached.urls));
+    return;
+  }
 
-    var pick = null;
+  const col = await db();
+  var servers = await col
+    .find(
+      { working: true },
+      {
+        projection: {
+          _id: 0,
+          lastOnline: 0,
+        },
+      }
+    )
+    .sort({
+      last_checked: -1,
+    })
+    .toArray();
 
-    while (pick === null && first_n.length > 0) {
-        console.log(first_n.length)
-        const server = first_n.shift();
-        const test_proxy_ret = await test_proxy(server.url);
-        if (test_proxy_ret) {
-            pick = server;
-        }
-    }
+  servers = servers.sort((a, b) => b.streak - a.streak);
+  const urls = servers.map((server) => server.url);
 
-    if (pick) {
-        const pac = write_proxy_pac(pick.url);
-        res.setHeader('Content-Type', 'application/x-ns-proxy-autoconfig');
-        res.setHeader("Content-Disposition", "attachment; filename=proxy.pac");
-        res.status(200).send(pac);
-    }
-    else {
-        res.status(404).send('No proxy found');
-    }
+  cached.urls = urls;
+  cached.last_updated = Date.now();
+
+  res.status(200).send(write_proxy_pac(urls));
 }
