544bd44d819cae897c81f31c81ecdb31ae984fc6
Midhun V Nadh <midhun.vnadh5732@gmail.com>
2024-12-01 16:21:35 +0530

    5 to 3

--- src/updater/get_working.js
+++ src/updater/get_working.js
@@ -1,12 +1,12 @@
 const db = require("../lib/db");
 
 const get_working = async () => {
-  const fiveMinutesAgo = new Date(Date.now() - 5 * 60 * 1000);
+  const ta = new Date(Date.now() - 3 * 60 * 1000);
   return await db.servers.findMany({
     where: {
       working: true,
       lastChecked: {
-        lt: fiveMinutesAgo,
+        lt: ta,
       },
     },
     take: 5,
